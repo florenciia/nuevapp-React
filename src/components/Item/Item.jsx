3a62bcb24f4a9f139dca103bbeea75561b9fcead
Florencia Tencha <38304663+florenciia@users.noreply.github.com>
2022-04-05 16:08:02 -0300

    inteno insertar imgs

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './Item.css';
 import { ItemCount } from '../ItemCount/ItemCount';
 
-const Item = ({id, titulo, img, precio}) => {
+const Item = ({id, titulo, imgg, precio}) => {
   return (
         <div className='container'>            
                         <div className='muestraPerfume'>
                                 <p className='id'> id: {id} </p>
                                 <h1 className='titulo'> {titulo} </h1>
-                                <img src={img} alt='images'/>
+                                <img src={imgg} alt='images'/>
                                 <p className='precio'> Precio: {precio} </p>
                                
                                 <ItemCount stock="10" initial="0" />
