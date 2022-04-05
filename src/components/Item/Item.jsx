528929880b8e249fc652fd611047dc7d4f6aa4b8
Florencia Tencha <38304663+florenciia@users.noreply.github.com>
2022-04-05 16:47:47 -0300

    imageenes subidas

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import './Item.css';
 import { ItemCount } from '../ItemCount/ItemCount';
 
-const Item = ({id, titulo, imgg, precio}) => {
+const Item = ({id, titulo, img, precio}) => {
   return (
         <div className='container'>            
                         <div className='muestraPerfume'>
                                 <p className='id'> id: {id} </p>
                                 <h1 className='titulo'> {titulo} </h1>
-                                <img src={imgg} alt='images'/>
+                                
+                                <div className='img-div'>
+                                  <img className='imgg' src={img} alt='images'/>
+                                </div>
+                                
                                 <p className='precio'> Precio: {precio} </p>
-                               
+                                
+                                <div className='btn-div'>
+                                  <button className='btn-details'> Ver detalles </button>
+                                </div>
+
                                 <ItemCount stock="10" initial="0" />
-                                <button className='btn-add'> Ver detalles </button>
+                                
+                                
                         </div>            
         </div>          
     );
